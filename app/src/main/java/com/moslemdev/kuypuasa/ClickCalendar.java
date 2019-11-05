6176275f982c0ebe001fcb853bf3b821817cb849
regalforlyfe <rifal.ahmd2@gmail.com>
2019-11-05 10:08:46 +0700

    add onclick calendar layout

---
+++ app/src/main/java/com/moslemdev/kuypuasa/ClickCalendar.java
@@ -0,0 +1,14 @@
+package com.moslemdev.kuypuasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClickCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click_calendar);
+    }
+}

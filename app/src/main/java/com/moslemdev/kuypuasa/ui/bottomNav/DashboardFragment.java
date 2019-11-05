76d61aaea4a146a4a4a25b3c16350d0229ddb43a
Alfarel Zaki <alfarell.zaki@gmail.com>
2019-11-05 10:26:27 +0700

    add onClick Calendar

--- app/src/main/java/com/moslemdev/kuypuasa/ui/bottomNav/DashboardFragment.java
+++ app/src/main/java/com/moslemdev/kuypuasa/ui/bottomNav/DashboardFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -16,10 +18,26 @@ import com.moslemdev.kuypuasa.R;
 
 public class DashboardFragment extends Fragment {
 
+    private CalendarView calendarView;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        calendarView = root.findViewById(R.id.calendar_view);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                Toast.makeText(getActivity().getApplicationContext(), ""+dayOfMonth, 0).show();
+            }
+        });
         return root;
+
     }
+
+
+
+
 }

46c08acc92a2f4fdc306b8c974139ec8b5a1a5a5
Alfarel Zaki <alfarell.zaki@gmail.com>
2019-11-23 13:17:23 +0700

    adding landing page UI

--- app/src/main/java/com/moslemdev/kuypuasa/Animation.java
+++ app/src/main/java/com/moslemdev/kuypuasa/Animation.java
@@ -2,6 +2,7 @@ package com.moslemdev.kuypuasa;
 
 import android.animation.Animator;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -24,7 +25,6 @@ public class Animation extends AppCompatActivity {
 
 
         loading = findViewById(R.id.loading_view);
-        loading.setRepeatCount(android.view.animation.Animation.RELATIVE_TO_PARENT);
         loading.playAnimation();
 
         loading.addAnimatorListener(new Animator.AnimatorListener(){
@@ -36,8 +36,16 @@ public class Animation extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animator animation) {
-                Intent i = new Intent(Animation.this, MainActivity.class);
-                startActivity(i);
+                SharedPreferences sharedPreferences
+                        = getSharedPreferences("ISI DATA DIRI", MODE_PRIVATE);
+                if (!sharedPreferences.contains("Sudah Terisi")) {
+                    SharedPreferences.Editor editor
+                            = sharedPreferences.edit();
+                    editor.putString("Sudah Terisi", "done!");
+                    editor.commit();
+                    Intent i = new Intent(Animation.this, LandingPage.class);
+                    startActivity(i);
+                }
                 finish();
             }
 
@@ -52,5 +60,6 @@ public class Animation extends AppCompatActivity {
             }
         });
     }
+
 }
 
---
+++ app/src/main/java/com/moslemdev/kuypuasa/LandingPage.java
@@ -0,0 +1,30 @@
+package com.moslemdev.kuypuasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class LandingPage extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+        getSupportActionBar().hide();
+
+        fab = findViewById(R.id.floating_action_button_landing_page);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LandingPage.this, IsiDataDiri.class);
+                startActivity(i);
+            }
+        });
+    }
+}

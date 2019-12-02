ccbf9624d2fc3b762591ebfe5c91a6192f8e6cbd
Alfarel Zaki <alfarell.zaki@gmail.com>
2019-12-03 02:33:52 +0700

    pop up

---
+++ app/src/main/java/com/moslemdev/kuypuasa/PopUpPuasaSunnah.java
@@ -0,0 +1,31 @@
+package com.moslemdev.kuypuasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
+
+public class PopUpPuasaSunnah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up_puasa_sunnah);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8), (int)(height*.7));
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+
+        getWindow().setAttributes(params);
+    }
+}
--- app/src/main/java/com/moslemdev/kuypuasa/ui/bottomNav/HomeFragment.java
+++ app/src/main/java/com/moslemdev/kuypuasa/ui/bottomNav/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.moslemdev.kuypuasa.ui.bottomNav;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,13 +13,54 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.card.MaterialCardView;
+import com.moslemdev.kuypuasa.PopUpPuasaSunnah;
 import com.moslemdev.kuypuasa.R;
 
 public class HomeFragment extends Fragment {
 
+    private MaterialCardView cvPuasaSunnah;
+    private MaterialCardView cvPuasaWajib;
+    private MaterialCardView cvPuasaMakruh;
+    private MaterialCardView cvPuasaHaram;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        cvPuasaSunnah = root.findViewById(R.id.card_view_puasa_sunnah);
+        cvPuasaWajib = root.findViewById(R.id.card_view_puasa_wajib);
+        cvPuasaMakruh = root.findViewById(R.id.card_view_puasa_makruh);
+        cvPuasaHaram = root.findViewById(R.id.card_view_puasa_haram);
+
+        cvPuasaSunnah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), PopUpPuasaSunnah.class);
+                startActivity(intent);
+            }
+        });
+
+        cvPuasaWajib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cvPuasaMakruh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cvPuasaHaram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         return root;
     }
 }

9c1bf823bfb86efc4ca672538425a27c41428d18
Presinutri <farizalbab@gmail.com>
2019-12-03 02:46:09 +0700

    deskripsi sudah dab

---
+++ app/src/main/java/com/moslemdev/kuypuasa/PuasaSeninKamisDeskripsi.java
@@ -0,0 +1,14 @@
+package com.moslemdev.kuypuasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PuasaSeninKamisDeskripsi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puasa_senin_kamis_deskripsi);
+    }
+}
